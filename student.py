5937ed4723b5511d2bbb447eafce4552f8515c6a
Jane Sieving <jsieving127@gmail.com>
2021-12-03 13:49:49 -0500

    Add combined skill scores for Students

--- student.py
+++ student.py
@@ -12,18 +12,23 @@ class Student:
 
         self.intr_mgmt = intr_mgmt
         self.exp_mgmt = exp_mgmt
+        self.mgmt = intr_mgmt + exp_mgmt
 
         self.intr_elec = intr_elec
         self.exp_elec = exp_elec
+        self.elec = intr_elec + exp_elec
 
         self.intr_prog = intr_prog
         self.exp_prog = exp_prog
+        self.prog = intr_prog + exp_elec
 
         self.intr_cad = intr_cad
         self.exp_cad = exp_cad
+        self.cad = intr_cad + exp_cad
 
         self.intr_fab = intr_fab
         self.exp_fab = exp_fab
+        self.fab = intr_fab + exp_fab
 
     def __repr__(self):
         return self.name
