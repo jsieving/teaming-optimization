0abe5a1de95784f98fb861f80cb00a967bd09183
Jane Sieving <jsieving127@gmail.com>
2021-12-03 14:43:47 -0500

    Add 2-student compatibility function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from data_loader import load_student_data
 from assignments import random_assignments
+from data_loader import load_student_data
+from helpers import compatibility
 from scoring import sample_score_func
 
 filename = input("Enter file name with survey data [team_sample_data.csv]: ") or "team_sample_data.csv"
 
 students = load_student_data(filename)
 print("First student: ", students[0])
+
 print("Students loaded: ", len(students))
 
 leftovers, teams = random_assignments(students)
@@ -13,3 +15,5 @@ leftovers, teams = random_assignments(students)
 print(teams)
 print("Score (higher is worse): %.2f" % sample_score_func(teams))
 print("%d students could not be assigned to teams." % len(leftovers))
+print(compatibility(*teams[0][:2]))
+print(compatibility(*students[:2]))
--- scoring.py
+++ scoring.py
@@ -10,3 +10,33 @@ def sample_score_func(teams):
         max_commitment = max([student.commitment for student in team])
         squared_errors_sum += (max_commitment - min_commitment) ** 2
     return math.sqrt(squared_errors_sum)
+
+
+def compatibility(student1, student2):
+    '''
+    Computes a compatibility score between 2 students.
+    If students have silver bullet between them, returns -infinity.
+    '''
+    if (student1.name in student2.silver_bullets or
+        student2.name in student1.silver_bullets):
+        return -float("inf")
+
+    commitment_spread = abs(student1.commitment - student2.commitment)
+    common_topics = len(student1.interests & student2.interests)
+    mutual_preference = ((student1.name in student2.preferences) +
+    (student2.name in student1.preferences))
+    skill_coverage = math.sqrt(
+        max(student1.mgmt, student2.mgmt)**2
+        + max(student1.elec, student2.elec)**2
+        + max(student1.prog, student2.prog)**2
+        + max(student1.cad,  student2.cad)**2
+        + max(student1.fab,  student2.fab)**2
+    )
+
+    scaled_commitment = (4-commitment_spread) / 4
+    scaled_topics = min(2, common_topics) / 2
+    scaled_preference = mutual_preference / 2
+    scaled_skill = skill_coverage / 16
+
+    return (4*scaled_commitment + 3*scaled_preference +
+        2*scaled_topics + scaled_skill)
