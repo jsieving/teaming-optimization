1a53b03fc605ae3e2e6eaabc446137c4a457da3c
Jane Sieving <jsieving127@gmail.com>
2021-12-13 03:22:20 -0500

    Add recursive backtracking attempt

--- assignments.py
+++ assignments.py
@@ -99,3 +99,70 @@ def assign_teams_random(four_cliques, five_cliques, n_4, n_5):
         start_at += 1
 
     return teams_of_4 + teams_of_5
+
+
+def assign_teams_rec(four_cliques, five_cliques, i, chosen_cliques, assigned_students, num_students, n):
+    num_students_left = num_students - len(assigned_students)
+    # If you can divide remaining students by 4, but not 5,
+    if (num_students_left % 4 == 0) and (num_students_left % 5 != 0):
+        # Start picking from 4-cliques
+        cliques = four_cliques
+        # And make it impossible to go back to 5-cliques
+        five_cliques = four_cliques
+    else:
+        cliques = five_cliques
+
+    # Base case: no more cliques to choose from
+    if i >= len(cliques):
+        return 10000, None
+    # Base case: no more cliques required -> return cost & choices
+    if n == 0:
+        print(0, chosen_cliques)
+        return assignment_cost(chosen_cliques), chosen_cliques[:]
+
+    # Additional base case: choose the next best clique of unassigned students
+    if n == 1:
+        if num_students-len(assigned_students) < 4:
+            # Can't create any more teams, so just score what you have
+            return assignment_cost(chosen_cliques), chosen_cliques[:]
+
+        j = i
+        # Find the first team of unassigned students, since they are sorted
+        # by score
+        while j < 1000 and overlaps(cliques[j].nodes, assigned_students):
+            j += 1
+        if j >= len(cliques):
+            # There are no cliques with the remaining students without silver
+            # bullets
+            return 10000, None
+
+        new_cliques = chosen_cliques + [cliques[j]]
+        # Calculate the overall score
+        cost = assignment_cost(new_cliques)
+        return cost, new_cliques
+
+    # Recursive case:
+    # Get current clique
+    curr_clique = cliques[i]
+    # Set costs for including or excluding very high by default
+    cost_incl, cost_excl = 10000, 10000
+    # Set both team options to None
+    teams_incl, teams_excl = None, None
+    # If ith clique does not overlap:
+    if not overlaps(curr_clique.nodes, assigned_students):
+        #   Get cost & choices for including ith clique
+        new_cliques = chosen_cliques + [curr_clique]
+        new_assigned_students = assigned_students | curr_clique.nodes
+        cost_incl, teams_incl = assign_teams_rec(
+            four_cliques, five_cliques, i+1, new_cliques, new_assigned_students, num_students, n-1)
+
+    if not teams_incl or team_compatibility(teams_incl[-1]) < 8:
+        # Get cost & choices for excluding ith clique
+        cost_excl, teams_excl = assign_teams_rec(
+            four_cliques, five_cliques, i+1, chosen_cliques, assigned_students, num_students, n)
+
+    if cost_excl > cost_incl:
+        cost, teams = cost_incl, teams_incl
+    else:
+        cost, teams = cost_excl, teams_excl
+    return cost, teams
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import joblib
 import time
 from networkx.classes import graph
-from assignments import assign_teams_greedy, assign_teams_random, assign_teams
+from assignments import (
+    assign_teams_greedy, assign_teams_random, assign_teams_rec)
 from helpers import num_size_teams
 from clique_finding import find_k_clique
 from scoring import (
-    assignment_cost,
-    team_compatibility,
-    team_evaluation
-)
+    assignment_cost, team_compatibility, team_evaluation)
 
 
 # Try to load a graph of students
@@ -26,7 +24,7 @@ except FileNotFoundError:
 four_cliques_filename = "data/four_cliques_" + student_graph_suffix
 try:
     four_cliques = joblib.load(four_cliques_filename)
-    print("%i four cliques loaded" % len(four_cliques))
+    print("%i 4-cliques loaded" % len(four_cliques))
 except FileNotFoundError:
     print(four_cliques_filename, "not found. Generating 4-cliques...")
     # Compute all possible 4-cliques
@@ -46,7 +44,7 @@ except FileNotFoundError:
 five_cliques_filename = "data/five_cliques_" + student_graph_suffix
 try:
     five_cliques = joblib.load(five_cliques_filename)
-    print("%i five cliques loaded" % len(five_cliques))
+    print("%i 5-cliques loaded" % len(five_cliques))
 except FileNotFoundError:
     print(five_cliques_filename, "not found. Generating 5-cliques...")
     # Compute all possible 5-cliques
@@ -86,25 +84,39 @@ num_5teams, num_4teams = num_size_teams(num_students)
 print("Students: %i; 4-teams: %i; 5-teams: %i" %
       (num_students, num_4teams, num_5teams))
 
-print("Running random assignments...")
-rand_teams = assign_teams_random(
-    four_cliques, five_cliques, num_4teams, num_5teams)
-print("Cost: (lower is better): %.3f" % assignment_cost(rand_teams))
-for team in rand_teams:
-    print(team.nodes, "\tCompat: %.2f; Eval: %.2f" %
-          (team_compatibility(team), team_evaluation(team)))
+# print("Running random assignments...")
+# rand_teams = assign_teams_random(
+#     four_cliques, five_cliques, num_4teams, num_5teams)
+# print("Cost: (lower is better): %.3f" % assignment_cost(rand_teams))
+# for team in rand_teams:
+#     print(team.nodes, "\tCompat: %.2f; Eval: %.2f" %
+#           (team_compatibility(team), team_evaluation(team)))
 
 
-print("Running greedy...")
-# Greedily assign required numbers of teams of 4 and 5
-greedy_teams = assign_teams_greedy(
-    four_cliques, five_cliques, num_4teams, num_5teams)
-print("Cost: (lower is better): %.3f" % assignment_cost(greedy_teams))
-for team in greedy_teams:
+# print("Running greedy...")
+# # Greedily assign required numbers of teams of 4 and 5
+# greedy_teams = assign_teams_greedy(
+#     four_cliques, five_cliques, num_4teams, num_5teams)
+# print("Cost: (lower is better): %.3f" % assignment_cost(greedy_teams))
+# for team in greedy_teams:
+#     print(team.nodes, "\tCompat: %.2f Eval: %.2f" %
+#           (team.graph['compat'], team_evaluation(team)))
+
+
+print("Running recursive backtracking...")
+
+# # Demote 4-cliques so 5s will be chosen first
+# for team in four_cliques:
+#     team.graph['compat'] = team.graph['compat'] - 1
+
+# # Make combination of all cliques
+# all_cliques = five_cliques + four_cliques
+# all_cliques.sort(key=lambda team: team.graph['compat'], reverse=True)
+
+cost, teams = assign_teams_rec(
+    four_cliques[:10000], five_cliques[:10000], 0, [], set(), num_students, num_5teams+num_4teams)
+
+print("Cost: (lower is better): %.3f" % cost)
+for team in teams:
     print(team.nodes, "\tCompat: %.2f Eval: %.2f" %
           (team.graph['compat'], team_evaluation(team)))
-
-
-# print("Running recursive backtracking...")
-# cost, teams = assign_teams(
-#     four_cliques[:100], 0, [], student_graph.nodes, 2)
